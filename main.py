a407576fce34fd644d4ef4c366380cd58e5bc22d
vito <ac1246897@gmail.com>
2024-02-12 12:10:30 +0800

    updated

---
+++ main.py
@@ -0,0 +1,22 @@
+import math, time
+import speedtest
+import os
+
+
+def bytes_to_MB(size_bytes):
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    power = math.pow(1024, i)
+    size = round(size_bytes / power, 2)
+    return f"{size} Mpbs"
+
+
+wifi = speedtest.Speedtest()
+
+print("Test is running...")
+print("Please wait for a moment!!!")
+download_speed = wifi.download()
+upload_speed = wifi.upload()
+
+print("Test is finished")
+print(f"Download speed:{bytes_to_MB(download_speed)}")
+print(f"Upload speed:{bytes_to_MB(upload_speed)}")
